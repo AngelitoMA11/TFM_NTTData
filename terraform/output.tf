b3e393d2e983b269f0a63e70f6eb1b9cd3bbb7ef
AngelitoMA11 <angelitoma200211@gmail.com>
2025-07-10 18:07:27 +0200

    Despliegue con el permiso que falta

--- terraform/output.tf
+++
@@ -1,7 +0,0 @@
-# output "workload_identity_provider" {
-#   value = module.gitaction.workload_identity_provider
-# }
-
-# output "service_account_email" {
-#   value = module.gitaction.service_account_email
-# }
